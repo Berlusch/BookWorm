c493cdefbe2ab31ac1941a9af37f82ae48508172
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-02-26 09:26:42 +0100

    Update BookTitle.cs

--- BookWorm.Model/BookTitle.cs
+++ BookWorm.Model/BookTitle.cs
@@ -1,16 +1,20 @@
-﻿using BookWorm.Model.Common;
+﻿using BookWorm.Model;
 
-namespace BookWorm.Model
+public class BookTitle : EntityBase, IBookTitle
 {
-    public class BookTitle : EntityBase, IBookTitle
-    {
-        public string Title { get; set; } = null!;
-        public string? Subtitle { get; set; }= null;
-        public string AuthorName { get; set; } = null!; 
-        public string LanguageName { get; set; } = null!; 
-        public string TagLineText { get; set; } = null!;
-        public TagLine TagLine { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string? Subtitle { get; set; }
 
-        public ICollection<Genre> Genres { get; set; } = new List<Genre>();
-    }
+    public int AuthorId { get; set; }
+    public Author Author { get; set; } = null!;
+
+    public int LanguageId { get; set; }
+    public Language Language { get; set; } = null!;
+
+    public TagLine TagLine { get; set; } = null!;
+    public ICollection<Genre> Genres { get; set; } = new List<Genre>();
+        
+    string IBookTitle.AuthorName => Author.FullName;
+    string IBookTitle.LanguageName => Language.Name;
+    string IBookTitle.TagLineText => TagLine.Text;
 }
