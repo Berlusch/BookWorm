7f91481366532882549fd882e0f10b111b11f256
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-02-27 10:59:16 +0100

    Create UnitOfWork and its interface

--- BookWorm.Repository.Common/IUnitOfWork.cs
+++ BookWorm.Repository.Common/IUnitOfWork.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BookWorm.Repository.Common
+﻿namespace BookWorm.Repository.Common
 {
-    internal interface IUnitOfWork
+    public interface IUnitOfWork: IDisposable
     {
+        Task BeginTransactionAsync();
+        Task CommitAsync();
+        Task RollbackAsync();
+
+        Task<int> SaveChangesAsync();
     }
 }
+
+
--- BookWorm.Repository/UnitOfWork.cs
+++ BookWorm.Repository/UnitOfWork.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BookWorm.Repository.Common;
+using BookWorm.DAL;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace BookWorm.Repository
 {
-    internal class UnitOfWork
+    public sealed class UnitOfWork(BookWormDbContext context) : IUnitOfWork
     {
+        private readonly BookWormDbContext _context =
+            context ?? throw new ArgumentNullException(nameof(context));
+
+        private IDbContextTransaction? _transaction;
+
+        public async Task BeginTransactionAsync()
+        {
+            _transaction = await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitAsync()
+        {
+            if (_transaction != null)
+            {
+                await _transaction.CommitAsync();
+            }
+        }
+
+        public async Task RollbackAsync()
+        {
+            if (_transaction != null)
+            {
+                await _transaction.RollbackAsync();
+            }
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _transaction?.Dispose();
+            _context.Dispose();
+
+            GC.SuppressFinalize(this);
+        }
     }
 }
+
