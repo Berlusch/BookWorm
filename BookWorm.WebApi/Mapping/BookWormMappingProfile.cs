e2100c83a737dd7fc687b306bc38bcdede15b2c6
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-03-03 13:38:54 +0100

    Update BookWormMappingProfile.cs

--- BookWorm.WebApi/Mapping/BookWormMappingProfile.cs
+++ BookWorm.WebApi/Mapping/BookWormMappingProfile.cs
@@ -1,18 +1,23 @@
-﻿using AutoMapper;
-using BookWorm.Model;
-using BookWorm.WebApi.DTO;
+﻿
+    using AutoMapper;
+    using BookWorm.Model;
+    using BookWorm.WebApi.DTO;
 
-namespace BookWorm.WebAPI.Mapping
-{
+    namespace BookWorm.WebAPI.Mapping
+
+{ 
     public class BookWormMappingProfile : Profile
     {
         public BookWormMappingProfile()
-        {            
+        {
+            
             CreateMap<Author, AuthorReadDto>()
                 .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.FullName));
                         
-            CreateMap<AuthorInsertUpdateDto, Author>();            
+            CreateMap<AuthorInsertUpdateDto, Author>();
+           
             CreateMap<Author, AuthorInsertUpdateDto>();
         }
     }
+
 }
