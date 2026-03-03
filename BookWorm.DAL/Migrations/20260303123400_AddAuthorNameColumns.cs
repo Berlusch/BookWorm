b0bb8a8e2660824b3512293a2e39fa71b1741798
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-03-03 13:37:28 +0100

    Add migration AddAuthorNameColumns

---
+++ BookWorm.DAL/Migrations/20260303123400_AddAuthorNameColumns.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookWorm.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuthorNameColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Authors");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Authors",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Authors",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Authors");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Authors",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
