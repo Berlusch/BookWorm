10a970451316693ec77e6ed85cc9af8204a73de4
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-03-12 11:46:10 +0100

    Add mappings for Language, Genre and BookTitle

--- BookWorm.WebApi/Mapping/BookWormMappingProfile.cs
+++ BookWorm.WebApi/Mapping/BookWormMappingProfile.cs
@@ -12,10 +12,21 @@
         {
 
             CreateMap<Author, AuthorReadDto>();
-
             CreateMap<AuthorInsertUpdateDto, Author>();
-
             CreateMap<Author, AuthorInsertUpdateDto>();
+
+            CreateMap<Language, LanguageReadDto>();
+            
+            CreateMap<Genre, GenreReadDto>();
+            CreateMap<GenreInsertUpdateDto, Genre>();
+
+            CreateMap<BookTitle, BookTitleReadDto>()
+                .ForMember(dest => dest.AuthorName, opt => opt.MapFrom(src => src.Author.FullName))
+                .ForMember(dest => dest.LanguageName, opt => opt.MapFrom(src => src.Language.Name))
+                .ForMember(dest => dest.TagLineText, opt => opt.MapFrom(src => src.TagLine.Text))
+                .ForMember(dest => dest.Genres, opt => opt.MapFrom(src => src.Genres.Select(g => g.Name).ToList()));
+
+            CreateMap<BookTitleInsertUpdateDto, BookTitle>();
         }
     }
 
