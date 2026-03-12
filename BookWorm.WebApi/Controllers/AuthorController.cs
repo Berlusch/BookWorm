18909df20c4b33bd3748b472d7e33681dfe9882e
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-03-12 09:49:29 +0100

    Refactor AuthorController to use PFSParametersFactory

--- BookWorm.WebApi/Controllers/AuthorController.cs
+++ BookWorm.WebApi/Controllers/AuthorController.cs
@@ -1,10 +1,10 @@
 ﻿using AutoMapper;
 using BookWorm.Common;
+using BookWorm.Common.Factories;
 using BookWorm.Model;
 using BookWorm.Service.Common;
 using BookWorm.WebApi.DTO;
 using Microsoft.AspNetCore.Mvc;
-
 namespace BookWorm.WebAPI.Controllers
 {
     [ApiController]
@@ -13,69 +13,69 @@ namespace BookWorm.WebAPI.Controllers
     {
         private readonly IAuthorService _authorService;
         private readonly IMapper _mapper;
-
         public AuthorController(IAuthorService authorService, IMapper mapper)
         {
             _authorService = authorService;
             _mapper = mapper;
         }
-                
-        [HttpGet]
-        public async Task<ActionResult<PagedResult<AuthorReadDto>>> GetAuthors([FromQuery] PFSParameters pfs)
-        {
-            var pagedAuthors = await _authorService.GetAuthorsAsync(pfs);
 
+        [HttpGet]
+        public async Task<ActionResult<PagedResult<AuthorReadDto>>> GetAuthors(
+            [FromQuery] int pageNumber = 1,
+            [FromQuery] int pageSize = 5,
+            [FromQuery] string orderBy = "Id",
+            [FromQuery] bool descending = false,
+            [FromQuery] string filterProperty = "",
+            [FromQuery] string filter = "")
+        {
+            var pfs = PFSParametersFactory.Create(pageNumber, pageSize, orderBy, descending, filterProperty, filter);
+
+            var pagedAuthors = await _authorService.GetAuthorsAsync(pfs);
             var dto = new PagedResult<AuthorReadDto>
             {
                 Items = pagedAuthors.Items.Select(a => _mapper.Map<AuthorReadDto>(a)).ToList(),
                 TotalCount = pagedAuthors.TotalCount,
                 Paging = pagedAuthors.Paging
             };
-
             return Ok(dto);
         }
-        
+
         [HttpGet("{id}")]
         public async Task<ActionResult<AuthorReadDto>> GetAuthorById(int id)
         {
             var author = await _authorService.GetAuthorByIdAsync(id);
             if (author == null)
                 return NotFound();
-
             var dto = _mapper.Map<AuthorReadDto>(author);
             return Ok(dto);
         }
-        
+
         [HttpPost]
         public async Task<ActionResult<AuthorReadDto>> AddAuthor([FromBody] AuthorInsertUpdateDto dto)
         {
             var author = _mapper.Map<Author>(dto);
             var added = await _authorService.AddAuthorAsync(author);
-
             var readDto = _mapper.Map<AuthorReadDto>(added);
             return CreatedAtAction(nameof(GetAuthorById), new { id = readDto.Id }, readDto);
         }
-        
+
         [HttpPut("{id}")]
         public async Task<ActionResult<AuthorReadDto>> UpdateAuthor(int id, [FromBody] AuthorInsertUpdateDto dto)
         {
             var author = _mapper.Map<Author>(dto);
             var updated = await _authorService.UpdateAuthorAsync(id, author);
-
             if (updated == null)
                 return NotFound();
-
             var readDto = _mapper.Map<AuthorReadDto>(updated);
             return Ok(readDto);
         }
-        
+
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteAuthor(int id)
         {
             var deleted = await _authorService.DeleteAuthorAsync(id);
             if (!deleted)
                 return NotFound();
-
             return NoContent();
         }
     }
