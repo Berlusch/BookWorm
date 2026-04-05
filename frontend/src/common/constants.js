1db0f2b672ccef561a101794e3bb0793a40ab508
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-04-05 11:17:14 +0200

    Initial frontend setup

---
+++ frontend/src/common/constants.js
@@ -0,0 +1,10 @@
+export const RouteNames = {
+    HOME: '/',
+    // route names
+}
+
+// Local development
+export const API_URL = 'https://localhost:7071/api/v1';
+
+// Production
+// export const API_URL = '';
