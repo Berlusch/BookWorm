d5d9a3ddc7fefd073700c6dee8936050814d6f60
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-04-07 09:14:36 +0200

    Add AuthorId, LanguageId and GenreIds to BookTitleReadDto

--- BookWorm.WebApi/DTO/BookTitleReadDto.cs
+++ BookWorm.WebApi/DTO/BookTitleReadDto.cs
@@ -1,13 +1,13 @@
-﻿namespace BookWorm.WebApi.DTO
+﻿public class BookTitleReadDto
 {
-    public class BookTitleReadDto
-    {
-        public int Id { get; set; }
-        public string Title { get; set; } = null!;
-        public string? Subtitle { get; set; }
-        public string AuthorName { get; set; } = null!;
-        public string LanguageName { get; set; } = null!;
-        public List<string> BookQuotes { get; set; } = new();
-        public List<string> Genres { get; set; } = new();
-    }
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string? Subtitle { get; set; }
+    public int AuthorId { get; set; }
+    public string AuthorName { get; set; } = null!;
+    public int LanguageId { get; set; }
+    public string LanguageName { get; set; } = null!;
+    public List<string> BookQuotes { get; set; } = new();
+    public List<string> Genres { get; set; } = new();
+    public List<int> GenreIds { get; set; } = new();
 }
